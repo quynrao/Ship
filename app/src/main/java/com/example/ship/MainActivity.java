d57cfc85971e760fdaca78263e2833f1e0b0f8fe
quynrao <gigiminmin@gmail.com>
2019-04-11 00:22:26 +0700

    createBill

--- app/src/main/java/com/example/ship/MainActivity.java
+++ app/src/main/java/com/example/ship/MainActivity.java
@@ -14,9 +14,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.create_bill);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
